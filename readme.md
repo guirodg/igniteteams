7beba972731867de7c23baf85396d00503374319
gui <guilherme.rddev@gmail.com>
2024-05-15 11:05:16 -0300

    Adiciona estrutura projeto, e ajuste tailwind

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Comandos utilizados
+
+npx create-expo-app name --template
+
+# TAILWIND
+
+npm i nativewind
+npm i -D tailwindcss@3.3.2
+REF: https://tailwindcss.com/docs/guides/create-react-app
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Groups() {
+  return (
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-red-600">Groups</Text>
+    </View>
+  );
+}
